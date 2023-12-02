f3f3a6f83428b821aca6cbb53be35ba4299d5367
DANIEL95AZ <lucio.aguirre@uabc.edu.mx>
2023-12-02 11:51:32 -0800

    Se incluyo CRUD para todos los modelos.

---
+++ Eportafolio/Controllers/ProyectosController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Eportafolio.Models;
+
+namespace Eportafolio.Controllers
+{
+    public class ProyectosController : Controller
+    {
+        private laguirreEntities db = new laguirreEntities();
+
+        // GET: Proyectos
+        public ActionResult Index()
+        {
+            return View(db.Proyectos.ToList());
+        }
+
+        // GET: Proyectos/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Proyectos proyectos = db.Proyectos.Find(id);
+            if (proyectos == null)
+            {
+                return HttpNotFound();
+            }
+            return View(proyectos);
+        }
+
+        // GET: Proyectos/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Proyectos/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Nombre,Descripcion,Imagen,Empresa,Fecha")] Proyectos proyectos)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Proyectos.Add(proyectos);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(proyectos);
+        }
+
+        // GET: Proyectos/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Proyectos proyectos = db.Proyectos.Find(id);
+            if (proyectos == null)
+            {
+                return HttpNotFound();
+            }
+            return View(proyectos);
+        }
+
+        // POST: Proyectos/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Nombre,Descripcion,Imagen,Empresa,Fecha")] Proyectos proyectos)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(proyectos).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(proyectos);
+        }
+
+        // GET: Proyectos/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Proyectos proyectos = db.Proyectos.Find(id);
+            if (proyectos == null)
+            {
+                return HttpNotFound();
+            }
+            return View(proyectos);
+        }
+
+        // POST: Proyectos/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Proyectos proyectos = db.Proyectos.Find(id);
+            db.Proyectos.Remove(proyectos);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
